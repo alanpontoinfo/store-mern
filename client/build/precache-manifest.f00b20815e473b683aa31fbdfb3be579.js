cbc1304487602de5e74d9e71b79fc2fe33ba6389
Estagiario <alanpontoinfo@gmail.com>
2024-03-01 12:03:09 -0300

    atualizando o build

---
+++ client/build/precache-manifest.f00b20815e473b683aa31fbdfb3be579.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4083a4b75761e3cf6052057d2d8ad95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50b0ec877909e3d04a06",
+    "url": "/static/css/main.3df08fc8.chunk.css"
+  },
+  {
+    "revision": "38e6e295405c1c8d5df2",
+    "url": "/static/js/2.25f416f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25f416f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b0ec877909e3d04a06",
+    "url": "/static/js/main.a03b3535.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/static/media/cart.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/close.2c5552a1.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/menu.b3f49afd.svg"
+  }
+]);
